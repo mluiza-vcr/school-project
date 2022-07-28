bdfb47c29d231e2406398620b8a4a2e4186a1dfb
Luiza Rodrigues <luiza.rodrigues@easytech.com.br>
2022-07-28 17:14:32 -0300

    Inicia projeto front-end

---
+++ src/App.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+import "./App.css";
+// import { SessionProvider } from "./context/SessionContext";
+// import { Switch, Router } from "react-router-dom";
+// import Routes from "./routes/index";
+
+function App() {
+  return (
+    <div className="App-header">
+      {/* <Router> */}
+        {/* <SessionProvider> */}
+          {/* <Switch> */}
+            {/* <Routes /> */}
+          {/* </Switch> */}
+        {/* </SessionProvider> */}
+      {/* </Router> */}
+    </div>
+  );
+}
+
+export default App;
